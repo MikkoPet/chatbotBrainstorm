095ecc99418bd8cd2be67384b8235e5b5dc46306
Sarah-Katz <Sarahkatz59@gmail.com>
2024-12-09 20:03:07 +0100

    fix: Register form unaccessible + no username in form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,8 +48,6 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
 
-            // do anything else you need here, like send an email
-
             return $security->login($user, 'form_login', 'login');
         }
 
